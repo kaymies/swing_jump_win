9d9fc66e6f14e1a8cabdd0c7eb3622a44d4388ef
ek315 <ek315@users.noreply.github.com>
2022-11-14 16:10:13 -0500

    changed start times

--- run_simulation.m
+++ run_simulation.m
@@ -27,10 +27,10 @@ z0 = [0.1318; -1.0472; 0.6454; -pi/2;...
 %         taus = BezierCurve(ctrl.Ts, t/ctrl.tfs);
 
 % set guess
-tf = 1;                                        % simulation final time
+tf = 2;                                        % simulation final time
 ctrl.tih = 1;
 ctrl.tfh = 1;                                  % control time points for hip - updated KS
-ctrl.Th = [20 20];                               % control values for hip - updated KS
+ctrl.Th = [0.1 0.1];                               % control values for hip - updated KS
 % ctrl.Th = [0 0];
 ctrl.tis = 1;
 ctrl.tfs = 1;                                  % control time points for shoulder - updated KS
@@ -124,6 +124,6 @@ ylabel('Energy')
 %%
 % Run the animation
 figure(3)                          % get the coordinates of the points to animate
-speed = 0.1;                                 % set animation speed
+speed = 0.5;                                 % set animation speed
 clf                                         % clear fig
 animate_simple(t,z,p,speed)                 % run animation
